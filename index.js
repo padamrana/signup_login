9d168304373a69a2a5d82a72fd06a003c6fa20ec
padamrana <105627018+padamrana@users.noreply.github.com>
2022-07-08 10:35:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose") ,
+     app = express() ;
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phno": phno,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_padam.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
+
